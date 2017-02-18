6d3209838d823fa5ed596e39b60bd5f55b6f709c
Lei Xu <komushi@gmail.com>
2017-02-18 09:33:20 +0900

    post offline worked. wip: post for aws

--- handler.js
+++ handler.js
@@ -2,6 +2,7 @@
 
 const getObject = require('./handlers/get-object.js');
 const putObject = require('./handlers/put-object.js');
+const postObject = require('./handlers/post-object.js');
 
 
 function makeGetResponse(error, result) {
@@ -41,6 +42,13 @@ module.exports.getObject = (event, context, callback) => {
 
 module.exports.putObject = (event, context, callback) => {
   putObject(event, (error, result) => {
+    const response = makeResponse(error, result)
+    context.succeed(response)
+  });
+};
+
+module.exports.postObject = (event, context, callback) => {
+  postObject(event, (error, result) => {
     const response = makeResponse(error, result)
     context.succeed(response)
   });
---
+++ handlers/post-object.js
@@ -0,0 +1,44 @@
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+
+// works with offline mode & aws mode
+module.exports = (event, callback) => {
+  console.log("event!!");
+  console.log(event);
+
+  var parseResult = safelyParseJSON(event.body);
+  var buffer;
+  if (parseResult.isJson) {
+    buffer = new Buffer(parseResult.payload['data']);
+  } else {
+    buffer = new Buffer(event.body, 'base64');
+  }
+
+  var params = 
+    {
+      Bucket: event.pathParameters.bucket,
+      Key: event.pathParameters.key,
+      ContentType: 'application/octet-stream',
+      Body: buffer
+    };
+
+  S3.putObject(params).promise().then(() => {
+    callback(null, {key: event.pathParameters.key});
+  }).catch(function(reason) {
+   callback(JSON.stringify(reason));
+  });
+}
+
+function safelyParseJSON (payload) {
+  // This function cannot be optimised, it's best to
+  // keep it small!
+  var parsed;
+
+  try {
+    parsed = JSON.parse(payload);
+  } catch (e) {
+    return {isJson: false, payload: payload};
+  }
+
+  return {isJson: true, payload: parsed}; // Could be undefined!
+}
